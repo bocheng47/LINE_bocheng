bedb573fa7bb40bb49f39bb19505ed6c48b38aac
bocheng <bocheng4o7@gmail.com>
2019-04-03 09:58:19 +0800

    new

--- app.py
+++ app.py
@@ -19,6 +19,17 @@ line_bot_api = LineBotApi('h36DEiW/iRpAgib0ryeNQmVAuKX6eaqEKzCj5QoT/2oXYXnRwFj9h
 # Channel Secret
 handler = WebhookHandler('d57f500b5ac8ede281c2b196e1ad547b')
 
+rich_menu_to_create = RichMenu(
+    size=RichMenuSize(width=2500, height=843),
+    selected=False,
+    name="Nice richmenu",
+    chat_bar_text="Tap here",
+    areas=[RichMenuArea(
+        bounds=RichMenuBounds(x=0, y=0, width=2500, height=843),
+        action=URIAction(label='Go to line.me', uri='https://line.me'))]
+)
+rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
+
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -45,48 +56,8 @@ def handle_follow(event):
 輸入：自我介紹、程式語言、工作經驗、GitHub
 
 或是透過下方選單選取喔～"""
-    
 
-    headers = {"Authorization":"Bearer 3Ma92PMIfy790Z...","Content-Type":"application/json"}
-
-    body = {
-        "size": {"width": 2500, "height": 1686},
-        "selected": "true",
-        "name": "bocheng-menu",
-        "chatBarText": "點我收合選單",
-        "areas":[
-            {
-                "bounds": {"x": 551, "y": 325, "width": 321, "height": 321},
-                "action": {"type": "message", "text": "up"}
-            },
-            {
-                "bounds": {"x": 876, "y": 651, "width": 321, "height": 321},
-                "action": {"type": "message", "text": "right"}
-            },
-            {
-                "bounds": {"x": 551, "y": 972, "width": 321, "height": 321},
-                "action": {"type": "message", "text": "down"}
-            },
-            {
-                "bounds": {"x": 225, "y": 651, "width": 321, "height": 321},
-                "action": {"type": "message", "text": "left"}
-            },
-            {
-                "bounds": {"x": 1433, "y": 657, "width": 367, "height": 367},
-                "action": {"type": "message", "text": "btn b"}
-            },
-            {
-                "bounds": {"x": 1907, "y": 657, "width": 367, "height": 367},
-                "action": {"type": "message", "text": "btn a"}
-            }
-        ]
-    }
-
-    req = requests.request('POST', 'https://api.line.me/v2/bot/richmenu', 
-                       headers=headers,data=json.dumps(body).encode('utf-8'))
-
-    print(req.text)
-
+    rich_menu = line_bot_api.get_rich_menu(rich_menu_id)
 
     button_template_message =ButtonsTemplate(
                                     thumbnail_image_url="https://i.imgur.com/mNnvVVe.jpg",
