d15878cd07b40df8f1ce5272b640af4bd1026861
bocheng <bocheng4o7@gmail.com>
2019-04-03 09:31:52 +0800

    new

--- app.py
+++ app.py
@@ -32,6 +32,19 @@ def callback():
         abort(400)
     return 'OK'
 
+rich_menu_to_create = RichMenu(
+    size=RichMenuSize(width=2500, height=843),
+    selected=False,
+    name="Nice richmenu",
+    chat_bar_text="Tap here",
+    areas=[RichMenuArea(
+        bounds=RichMenuBounds(x=0, y=0, width=2500, height=843),
+        action=URIAction(label='Go to line.me', uri='https://line.me')
+    )]
+)
+rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
+print(rich_menu_id)
+
 # 預設訊息
 @handler.add(FollowEvent)
 def handle_follow(event):
